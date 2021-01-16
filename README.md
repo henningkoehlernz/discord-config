86fe0f022dc303c303c835fe8a22bb237b6452a6
Henning Koehler <henning.koehler.nz@gmail.com>
2021-01-17 12:28:14 +1300

    renamed commands; added ccmatch; ccadd checks for existing roles

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Discord Config
+# Discord Course Config
 
 Bot for setting up discord channels via commands. Designed for Massey CSIT server, which has lots of similar categories, one for each course.
 
@@ -13,27 +13,31 @@ Bot for setting up discord channels via commands. Designed for Massey CSIT serve
 # Commands
 
 The following commands are available (any server channel will work, requires Administrator permission):
-* !addcourse <course_names>
+* `!ccadd <course_names>`
 
 Will add roles & channels for the listed courses. Course names must start with a 6-digit number, and are separated by a newline character. Example:
 ```
-!addcourse
+!ccadd
 159271 Computational Thinking
 158247 Database Design
 ```
 
 *Note: This must be entered as a single command. Copy & paste or use `<shift-enter>`.*
 
-* !delcourse <course_numbers>
+* `!ccdelete <course_numbers>`
 
 Will delete matching roles & channels. Course numbers must either be a single 6-digit number, or a 0-5 digit number followed by `*`. Examples:
 ```
-!delcourse 159271
-!delcourse 159*
-!delcourse *
+!ccdelete 159271
+!ccdelete 159*
+!ccdelete *
 ```
 
 *Note: Matching will be done via role names. Only 6-digit role names are considered, so non-course channels will never be deleted.*
+
+* `!ccmatch <course_numbers>`
+
+Like `!ccdelete` except that it only lists matching courses without deleting them.
 
 # Other
 
--- discord-config.py
+++ discord-config.py
@@ -20,8 +20,7 @@ def get_course_nr(course: str):
 def is_course_name(course: str):
     return is_course_nr(get_course_nr(course))
 
-async def check_course(ctx, course: str):
-    # courses are 6-digit numbers
+async def check_course_name(ctx, course: str):
     if not is_course_name(course):
         await ctx.send("'{}' is not a valid course name. Must start with a 6-digit number.".format(course))
         return False
@@ -44,23 +43,29 @@ async def on_ready():
 
 @bot.command()
 @commands.has_permissions(administrator=True)
-async def addcourse(ctx, *, courses: str):
-    for course in courses.split("\n"):
-        if await check_course(ctx, course):
+async def ccadd(ctx, *, courses: str):
+    for course_name in courses.split("\n"):
+        if await check_course_name(ctx, course_name):
+            course_nr = get_course_nr(course_name)
+            # check if role already exists
+            existing = [ role for role in ctx.guild.roles if role.name == course_nr ]
+            if existing:
+                await ctx.send("Role {} already exists.".format(course_nr))
+                continue
             # create role & category
-            role = await ctx.guild.create_role(name=get_course_nr(course))
-            category = await ctx.guild.create_category(name=course)
+            role = await ctx.guild.create_role(name=course_nr)
+            category = await ctx.guild.create_category(name=course_name)
             # configure permissions
             await category.set_permissions(role, view_channel=True)
             await category.set_permissions(ctx.guild.default_role, view_channel=False)
             # create default channels
             for channel in course_channels:
                 await category.create_text_channel(name=channel)
-            await ctx.send("Created role & channels for {}".format(course))
+            await ctx.send("Created role & channels for {}".format(course_name))
 
 @bot.command()
 @commands.has_permissions(administrator=True)
-async def delcourse(ctx, course_pattern: str):
+async def ccdelete(ctx, course_pattern: str):
     courses = await filter_courses(ctx, course_pattern)
     if courses == None:
         return
@@ -75,6 +80,15 @@ async def delcourse(ctx, course_pattern: str):
     # list full channel names for easy recreation
     deleted = [ c.name for c in categories ]
     await ctx.send("Deleted roles & channels for\n{}".format("\n".join(deleted)))
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def ccmatch(ctx, course_pattern: str):
+    courses = await filter_courses(ctx, course_pattern)
+    if courses == None:
+        return
+    categories = [ c.name for c in ctx.guild.categories if get_course_nr(c.name) in courses ]
+    await ctx.send("{} matches the following:\n{}".format(course_pattern, "\n".join(categories)))
 
 # run the bot - token is stored in separate file to avoid accidental check-in
 with open('token.txt') as f:
